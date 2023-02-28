b377799edb2035aa23f215fe6d7916fdf377ba8c
kmj0505 <menge0505@naver.com>
2023-02-28 19:49:08 +0900

    Q1. Write UserInfo

---
+++ day9/exam/answer1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+	string name, link;
+	int age;
+	cout << "이름 : ";
+	cin >> name;
+	cout << "나이 : ";
+	cin >> age;
+	cout << "Link URL : ";
+	cin >> link;
+	cout << "\"" << link << "\"";
+
+	return 1;
+}
